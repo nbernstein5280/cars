152510f7b436d59fce2f6a19707156205d23c533
nbernstein5280 <nbernstein@chapman.edu>
2020-12-06 20:57:50 -0700

    Adds all files

---
+++ cars.sh
@@ -0,0 +1,19 @@
+num="0"
+
+while [[ "$num" -lt "3" ]]
+do
+	echo -n "Type 1 to enter a new car, 2 to list the cars, or 3 to quit: "
+	read -r num
+	case "$num" in
+		"1" )
+			echo -n "Enter the year the car was made: "; read -r year
+			echo -n "Enter the make of car: "; read -r make
+			echo -n "Enter the model of car: "; read -r model
+			string="$year:$make:$model"
+			echo "$string" >> My_old_cars;;
+		"2" )
+			sort My_old_cars;;
+		"3" )
+			echo "Thanks!";;
+	esac 
+done
